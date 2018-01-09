a0f7963d38bc76a21bad8d536caf1c0340933b54
Pranoy Kumar Kundu <pranoy1998k@gmail.com>
2018-01-09 17:01:29 +0530

    request.log added which stores all required requests by client

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const timeStamp = require('./time.js').timeStamp;
 const WebApp = require('./webApp.js');
 const loginPage = fs.readFileSync('./login.html');
 
+let toS = o=>JSON.stringify(o,null,2);
+
+let logRequest = (req,res)=>{
+  let text = ['------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${toS(req.headers)}`,
+    `COOKIES=> ${toS(req.cookies)}`,
+    `BODY=> ${toS(req.body)}`,''].join('\n');
+  fs.appendFile('request.log',text,()=>{});
+
+  console.log(`${req.method} ${req.url}`);
+}
+
 let registered_users = [{userName:'pranoyk', name:'Pranoy'}, {userName:'kaskichandrakant', name:'Chandrakant'}];
 
 let loadUser = (req,res)=>{
@@ -26,11 +40,6 @@ const getHeaderType = (file)=>{
     '.js' : 'text/js'
   }
   return headers[extension];
-}
-
-let redirectToHome = (req,res)=>{
-  console.log(req.url);
-  if(req.urlIsOneOf(['/']) ) res.redirect('/index.html');
 }
 
 const isFile = (path)=>{
@@ -82,6 +91,7 @@ let redirectLoggedOutUserToLogin = (req,res)=>{
 }
 
 let app = WebApp.create();
+app.use(logRequest);
 app.use(loadUser);
 app.use(serveStaticFile);
 app.use(redirectLoggedOutUserToLogin);
